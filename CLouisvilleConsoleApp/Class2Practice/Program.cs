b974a99fba7fb2d370fe0237dad6e5ad766e7b90
TMinch <Trent.Minch@eblusolutions.com>
2022-05-24 19:34:27 -0400

    Add a few more examples

---
+++ CLouisvilleConsoleApp/Class2Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Class3Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While loop example
+            Console.WriteLine("Please enter a number: ");
+            string input = Console.ReadLine();
+            int inputAsInt;
+            bool wasInt = int.TryParse(input, out inputAsInt);
+            while(!wasInt) //While the user's input was NOT an integer...
+            {
+                Console.WriteLine("That wasn't a number! Please try again.");
+                Console.WriteLine("Please enter a number: ");
+                input = Console.ReadLine();
+                wasInt = int.TryParse(input, out inputAsInt);
+            }
+            
+            Console.WriteLine($"You entered {inputAsInt}");
+                
+        }
+    }
+}
